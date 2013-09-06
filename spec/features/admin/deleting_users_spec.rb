b1a11d362cfcce59ae6a86e187f2acf471dfb579
Steve Klabnik <steve@steveklabnik.com>
2013-09-06 12:29:28 -0700

    Add feature for deleting users,              including protection against self-deletion

---
+++ spec/features/admin/deleting_users_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+feature 'Deleting users' do
+  let!(:admin_user) { FactoryGirl.create(:admin_user) }
+  let!(:user) { FactoryGirl.create(:user) }
+
+  before do
+    sign_in_as!(admin_user)
+    visit '/'
+
+    click_link 'Admin'
+    click_link 'Users'
+  end
+
+  scenario "Deleting a user" do
+    click_link user.email
+    click_link "Delete User"
+
+    expect(page).to have_content("User has been deleted")
+  end
+
+  scenario "Users cannot delete themselves" do
+    click_link admin_user.email
+    click_link "Delete User"
+
+    expect(page).to have_content("You cannot delete yourself!")
+  end
+end
