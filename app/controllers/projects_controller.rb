6559b161337f1901f0633294e7544537b13290b9
Steve Klabnik <steve@steveklabnik.com>
2013-09-05 17:40:58 -0700

    Implement delete functionality for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -38,6 +38,15 @@ class ProjectsController < ApplicationController
     end
   end
 
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+
+    flash[:notice] = "Project has been destroyed."
+
+    redirect_to projects_path
+  end
+
   private
     
     def project_params
---
+++ spec/features/deleting_projects_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+feature "Deleting projects" do
+  scenario "Deleting a project" do
+    FactoryGirl.create(:project, name: "TextMate 2")
+
+    visit "/"
+    click_link "TextMate 2"
+    click_link "Delete Project"
+
+    expect(page).to have_content("Project has been destroyed.")
+
+    visit "/"
+
+    expect(page).to have_no_content("TextMate 2")
+  end
+end
