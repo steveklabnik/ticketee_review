a02b56bcc6e1b8541fe22f1b9345f31277cf900e
Steve Klabnik <steve@steveklabnik.com>
2013-09-06 16:04:25 -0700

    Restrict ticket updating to only                those who have permission

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -3,6 +3,7 @@ class TicketsController < ApplicationController
   before_action :set_project
   before_action :set_ticket, only: [:show, :edit, :update, :destroy]
   before_action :authorize_create!, only: [:new, :create]
+  before_action :authorize_update!, only: [:edit, :update]
 
   def new
     @ticket = @project.tickets.build
@@ -63,6 +64,13 @@ class TicketsController < ApplicationController
     def authorize_create!
       if !current_user.admin? && cannot?("create tickets".to_sym, @project)
         flash[:alert] = "You cannot create tickets on this project."
+        redirect_to @project
+      end
+    end
+
+    def authorize_update!
+      if !current_user.admin? && cannot?("edit tickets".to_sym, @project)
+        flash[:alert] = "You cannot edit tickets on this project."
         redirect_to @project
       end
     end
--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -30,6 +30,12 @@ describe TicketsController do
       flash[:alert].should eql(message)
     end
 
+    def cannot_update_tickets!
+      expect(response).to redirect_to(project)
+      expect(flash[:alert]).to eql("You cannot edit tickets " \
+                                   "on this project.")
+    end
+
     it "cannot begin to create a ticket" do
       get :new, project_id: project.id
       cannot_create_tickets!
@@ -38,6 +44,19 @@ describe TicketsController do
     it "cannot create a ticket without permission" do
       post :create, project_id: project.id
       cannot_create_tickets!
+    end
+
+    it "cannot edit a ticket without permission" do
+      get :edit, { project_id: project.id, id: ticket.id }
+      cannot_update_tickets!
+    end
+
+    it "cannot update a ticket without permission" do
+      put :update, { project_id: project.id,
+                     id: ticket.id,
+                     ticket: {}
+                   }
+      cannot_update_tickets!
     end
   end
 end
