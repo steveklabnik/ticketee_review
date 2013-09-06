5be9cc2075059e4bb1af7c3e0572eb69f5344043
Steve Klabnik <steve@steveklabnik.com>
2013-09-06 11:41:39 -0700

    added admin flag to user.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130906181001) do
+ActiveRecord::Schema.define(version: 20130906183849) do
 
   create_table "projects", force: true do |t|
     t.string   "name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20130906181001) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",           default: false
   end
 
 end
--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -4,5 +4,9 @@ FactoryGirl.define do
     email "sample@example.com"
     password "hunter2"    
     password_confirmation "hunter2"    
+
+    factory :admin_user do
+      admin true
+    end
   end
 end
