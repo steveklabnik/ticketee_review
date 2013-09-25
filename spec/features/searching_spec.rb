8b938546218d6f53410f9d103dcd731c7e8ef9d1
Steve Klabnik <steve@steveklabnik.com>
2013-09-25 14:46:12 -0700

    Users can now click a tag's name to go to a page showing all tickets for it.

--- spec/features/searching_spec.rb
+++ spec/features/searching_spec.rb
@@ -49,5 +49,13 @@ feature "Searching" do
       expect(page).to_not have_content("Create users")
     end
   end
+
+  scenario "Clicking a tag goes to search results" do
+    click_link "Create projects"
+    click_link "iteration_1"
+    within("#tickets") do
+      expect(page).to have_content("Create projects")
+      expect(page).to_not have_content("Create users")
+    end
+  end
 end
-      
