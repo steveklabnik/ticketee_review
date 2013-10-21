6fee4ef499c0a07a43c8e339e1ebd7cf2688bdfb
Steve Klabnik <steve@steveklabnik.com>
2013-10-21 20:30:54 +0200

    Set up application to connect to Gmail to send emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,14 @@ Ticketee::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 end
